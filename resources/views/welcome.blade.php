233d2be477965ca2aa458b5da8e705c0d2b12fdc
ARCHNEMESIS2423 <jmkolanguzi@gmail.com>
2026-09-11 22:22:04 +0300

    made changes to package.json and css file removed unnecesary flux-css-stuff

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,9 +18,3 @@
         </header>
     </body>
 </html>
-
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/ARCHNEMESIS2423/sigichuma-app.git
-git push -u origin main
